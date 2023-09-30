6218e8c3f5a38cb4b6b82c60d7adf5242f408941
Manmay <manmay.singh@gmail.com>
2023-09-30 12:12:03 +0530

    added cors

--- src/__init__.py
+++ src/__init__.py
@@ -1,9 +1,11 @@
 from flask import Flask
 from flask_restful import Api
 from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
 
 app = Flask(__name__)
 api = Api(app)
+CORS(app)
 
 app.config["SECRET_KEY"] = "LSYTH37654323QFSA5T535DFQW3gt67we52q3hjm.jop;ui7"
 
